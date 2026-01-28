1fe4246689d63c7f8e11c4a71d09d1e3ff5a2295
Abdulaziz871 <aswadies.87@gmail.com>
2026-01-28 17:00:49 +0800

    Fix: Add TypeScript ignore for wowjs module to resolve Vercel build error

--- components/WowInit.tsx
+++ components/WowInit.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 export default function WowInit() {
     useEffect(() => {
         // Dynamically import WOW.js only on client side
+        // @ts-ignore - wowjs doesn't have TypeScript declarations
         const initWow = async () => {
             const WOW = (await import('wowjs')).WOW;
             new WOW({
